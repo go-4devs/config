302af61012430865a67885244771f451f395bbb3
andrey <andrey@4devs.io>
2025-12-29 22:18:13 +0300

    set definition as option

--- definition/defenition.go
+++ definition/defenition.go
@@ -2,15 +2,21 @@ package definition
 
 import (
 	"gitoa.ru/go-4devs/config"
+	"gitoa.ru/go-4devs/config/param"
 )
+
+var _ config.Group = (*Definition)(nil)
 
 func New(opts ...config.Option) *Definition {
 	return &Definition{
 		options: opts,
+		Params:  param.New(),
 	}
 }
 
 type Definition struct {
+	param.Params
+
 	options []config.Option
 }
 
@@ -20,4 +26,19 @@ func (d *Definition) Add(opts ...config.Option) {
 
 func (d *Definition) Options() []config.Option {
 	return d.options
+}
+
+func (d *Definition) Name() string {
+	return ""
+}
+
+func (d *Definition) With(params param.Params) *Definition {
+	def := &Definition{
+		options: make([]config.Option, len(d.options)),
+		Params:  param.Chain(params, d.Params),
+	}
+
+	copy(def.options, d.options)
+
+	return def
 }
