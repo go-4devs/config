dac9a87743754880818d915260de65cfa9ab2219
andrey <andrey@4devs.io>
2026-01-02 22:40:23 +0300

    param add helper get rune

--- param/helper.go
+++ param/helper.go
@@ -11,6 +11,17 @@ func String(fn Params, key any) (string, bool) {
 	return data, ok
 }
 
+func Rune(fn Params, key any) (rune, bool) {
+	val, ok := fn.Param(key)
+	if !ok {
+		return '0', false
+	}
+
+	data, dok := val.(rune)
+
+	return data, dok
+}
+
 func Bool(key any, fn Params) (bool, bool) {
 	val, ok := fn.Param(key)
 	if !ok {
